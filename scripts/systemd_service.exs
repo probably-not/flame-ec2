52a69562f00effbcee78f331d019ba1296718d80
Coby Benveniste <coby.benveniste@gmail.com>
2025-01-02 12:08:51 +0200

    Allow using a custom command so we can shellcheck and analyze the examples

--- scripts/systemd_service.exs
+++ scripts/systemd_service.exs
@@ -1,3 +1,3 @@
-rendered = FlameEC2.Templates.systemd_service(app: "flame_ec2")
+rendered = FlameEC2.Templates.systemd_service(app: "flame_ec2", custom_start_command: "ls", custom_stop_command: "ls")
 
 File.write!("./example_systemd_service.service", rendered)

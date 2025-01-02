e0832dc6b65906f167ad543d049875edc643fce9
Coby Benveniste <coby.benveniste@gmail.com>
2025-01-02 11:32:12 +0200

    Initialize template module for rendering the templates of the systemd and start scripts

---
+++ lib/flame_ec2/templates.ex
@@ -0,0 +1,10 @@
+defmodule FlameEC2.Templates do
+  @moduledoc false
+
+  require EEx
+
+  @templates_path Path.absname("./templates", __DIR__)
+
+  EEx.function_from_file(:def, :systemd_service, Path.join(@templates_path, "systemd.service.eex"), [:assigns])
+  EEx.function_from_file(:def, :start_script, Path.join(@templates_path, "start.sh.eex"), [:assigns])
+end

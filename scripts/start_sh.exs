c7ea88bbe88db89c2717448e04f7898385aa17b7
Coby Benveniste <coby.benveniste@gmail.com>
2025-01-02 12:51:01 +0200

    Add template for start.sh initialization script

--- scripts/start_sh.exs
+++ scripts/start_sh.exs
@@ -1,5 +1,12 @@
 systemd_service = FlameEC2.Templates.systemd_service(app: "flame_ec2")
 
-rendered = FlameEC2.Templates.start_script(app: "flame_ec2", systemd_service: systemd_service)
+rendered =
+  FlameEC2.Templates.start_script(
+    app: "flame_ec2",
+    systemd_service: systemd_service,
+    aws_region: "us-east-1",
+    s3_bundle_url: "s3://code.tar.gz",
+    s3_bundle_compressed: true
+  )
 
 File.write!("./example_start.sh", rendered)
